611d2074e441e0041bdca8934a62ab049d521add
jmpfbmx <42691741+jmpfbmx@users.noreply.github.com>
2019-12-27 23:05:50 +0100

    Adding some camera libraries

--- device.mk
+++ device.mk
@@ -101,6 +101,8 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
+    android.hardware.camera.provider@2.4-impl \
+    camera.device@1.0-impl \
     Snap
 
 # Connectivity Engine support (CNE)

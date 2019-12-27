f85ac63d7a594cc7b7bf9e486b52f347e8eb9c01
jmpfbmx <42691741+jmpfbmx@users.noreply.github.com>
2019-12-27 23:07:53 +0100

    Adding DRM lib

--- device.mk
+++ device.mk
@@ -125,6 +125,10 @@ PRODUCT_PACKAGES += \
     liboverlay \
     memtrack.msm8952
 
+# DRM
+PRODUCT_PACKAGES += \
+    android.hardware.drm@1.0-impl
+
 # Fingerprint
 PRODUCT_PACKAGES += \
     fingerprintd
